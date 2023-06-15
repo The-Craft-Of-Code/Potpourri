5123a7e1614007262eb912926b1a7b2c96ad7496
sooryasrajan <soorya.s27@gmail.com>
2023-06-15 21:57:02 +0530

    Added files

---
+++ src/components/selectedworks/data.js
@@ -0,0 +1,42 @@
+export const works = [
+  {
+    title: "Work 1",
+    description: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl."
+    ],
+  },
+  {
+    title: "Work 2",
+    description: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl."
+    ],
+  },
+  {
+    title: "Work 3",
+    description: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl."
+    ],
+  },
+  {
+    title: "Work 4",
+    description: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl."
+    ],
+  },
+  {
+    title: "Work 5",
+    description: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl. Sed euismod, nisl quis aliquam ultricies, nunc nisl ultricies nunc, nec aliquam nisl nisl nec nisl."
+    ],
+  }
+]
