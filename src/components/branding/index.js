c691a608c9601abadb85ad55ac7f354d2bebc65a
sooryasrajan <soorya.s27@gmail.com>
2023-06-18 22:45:45 +0530

    Added branding container content and logic.

--- src/components/branding/index.js
+++ src/components/branding/index.js
@@ -1,14 +1,86 @@
 import styled from "styled-components";
+import {colors} from "../../colors";
 
 export const RowContainer = styled.div`
   display: flex;
   flex-direction: row;
   margin: 3em;
-  background-color: red;
-  max-height: 55%;
+  max-height: 40%;
   flex: 3;
 `
 
-export const BrandingContainer = styled.div`
+export const LeftImage = styled.img`
+  display: flex;
+  flex: 2;
+  max-width: 40%;
+`
+
+export const ListButton = styled.div`
+  width: 100%;
+  padding: 1em;
+  border-bottom: 1px solid #000;
+  font-size: calc(0.8rem + 0.8vw);
+  font-family: Shocka Serif, serif;
+  transition: all 0.2s ease-in-out;
   
+  &:hover {
+    cursor: pointer;
+    color: ${colors.primaryColor};
+  }
+`
+
+export const RightContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 3;
+  justify-content: space-between;
+  height: 100%;
+  margin-left: 3em;
+`
+
+export const BrandingContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`
+
+export const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 3em;
+  padding-right: 3em;
+`
+
+export const BrandListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  background-color: #FECCC3;
+  overflow-x: hidden;
+  flex: 1;
+  align-items: center;
+`
+
+export const MarqueeContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  --gap: 5rem;
+  user-select: none;
+  gap: var(--gap);
+  animation: scroll 20s linear infinite;
+  @keyframes scroll {
+    from {
+      transform: translateX(30%);
+    }
+    to {
+      transform: translateX(-30%);
+    }
+  }
+
+`
+
+export const BrandingText = styled.h1`
+  padding: 0;
+  font-size: calc(60px + 2vw);
+  color: #fd9987;
+  gap: var(--gap);
 `
--- src/pages/BrandingPage.js
+++ src/pages/BrandingPage.js
@@ -1,8 +1,14 @@
 import React, {useEffect, useRef} from "react";
-import {Container} from "../components/root";
+import {Container, PrimaryTitle, SecondaryTitle} from "../components/root";
 import useElementOnScreen from "../utils/useOnScreen";
-import {BrandingContainer, RowContainer} from "../components/branding";
-import {RightContainerBottom as Image} from "../components/standout";
+import {
+  BrandingContainer, BrandingText, BrandListContainer,
+  LeftImage,
+  ListButton, MarqueeContainer,
+  RightContainer,
+  RowContainer,
+  TitleContainer
+} from "../components/branding";
 import blurImage from "../assets/images/blur-mesh.png";
 
 export const BrandingPage = (props) => {
@@ -16,13 +22,64 @@ export const BrandingPage = (props) => {
     }
   }, [isBrandingVisible, props]);
 
+  const branding = ["abc", "def", "ghi", "jkl", "mno", "pqr", "stu", "vwx", "yz"];
+
   return (
     <Container ref={brandingRef}>
       <RowContainer>
-        <Image/>
+        <LeftImage src={blurImage}/>
+        <RightContainer>
+          <ListButton>
+            How can we help make an impact?
+          </ListButton>
+          <ListButton>
+            How to get in touch with us?
+          </ListButton>
+          <ListButton>
+            How can you be sure?
+          </ListButton>
+        </RightContainer>
       </RowContainer>
       <BrandingContainer>
-        branding
+        <TitleContainer>
+          <PrimaryTitle>
+            Collaborating
+          </PrimaryTitle>
+          <SecondaryTitle>
+            Brands
+          </SecondaryTitle>
+        </TitleContainer>
+        <BrandListContainer>
+          <MarqueeContainer>
+            {
+              branding.map((brand, index) => {
+                return (
+                  <BrandingText key={index}>
+                    {brand}
+                  </BrandingText>
+                )
+              })
+            }
+            {
+              branding.map((brand, index) => {
+                return (
+                  <BrandingText key={index}>
+                    {brand}
+                  </BrandingText>
+                )
+              })
+            }
+            {
+              branding.map((brand, index) => {
+                return (
+                  <BrandingText key={index}>
+                    {brand}
+                  </BrandingText>
+                )
+              })
+            }
+          </MarqueeContainer>
+        </BrandListContainer>
       </BrandingContainer>
     </Container>
   )
