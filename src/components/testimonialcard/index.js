3015caf97bd06a1f7b1d73a2a06db3642f2b5e33
sooryasrajan <soorya.s27@gmail.com>
2023-06-24 00:35:36 +0530

    Made testimonial card more responsive.

--- src/components/testimonialcard/index.js
+++ src/components/testimonialcard/index.js
@@ -6,7 +6,6 @@ export const TestimonialCardContainer = styled.div`
   flex: 1;
   background-color: white;
   width: 100%;
-  height: 100%;
   border-radius: 15px;
   box-shadow: 0 0 10px 0 rgba(152, 152, 152, 0.2);
   padding: 1.5em;
