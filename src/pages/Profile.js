52d9239c92b1b0e21b3c8e86f9019157f7642349
Nirmal K <nirmalkarthikeyan1503@gmail.com>
2023-07-11 14:18:05 +0530

    udpated testimonial content

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -30,29 +30,38 @@ export function Profile(props) {
 
   let testimonialData = [
     {
-      name: "John Doe",
-      employmentTitle: "Manager at Company Name",
+      name: "Teja",
+      employmentTitle: "",
       reviewTitle: "Great service!",
-      reviewBody: "Some great comment about the services offered by the agency. Maybe talk about the quality or quickness or something of that sort. Crisp, interesting comment. ",
-      brandName: "Brand name user",
+      reviewBody: "Snigdha's exceptional ability to comprehend our requirements and capture the essence of our brand was truly impressive. Her innovative ideas and imaginative approach brought a breath of fresh air to our branding and content strategy. Since she joined our team, we noticed a remarkable transformation in how people perceive Capricio's brand and content, significantly improving our online presence.      ",
+      brandName: "Team Caprico",
       date: "01/01/2021",
       profileImage: "https://www.w3schools.com/howto/img_avatar.png"
     },
     {
-      name: "Ethan Hunt",
-      employmentTitle: "Manager at some other company",
+      name: "Lavanya",
+      employmentTitle: "",
       reviewTitle: "Amazing service!",
-      reviewBody: "Some great comment about the services offered by the agency. Maybe talk about the quality or quickness or something of that sort. Crisp, interesting comment. ",
-      brandName: "Brand name user",
+      reviewBody: "She is a talented, enthusiastic, and versatile writer who helped us start our blog journey. As a startup, working with independent content writers was the first choice, and working with Snigdha helped us more than we could expect. She is so quick in understanding our requirements and delivering the content in the shortest possible time. We appreciate her ability to create content on various titles across our businesses.",
+      brandName: "Behold.Social",
       date: "01/01/2023",
       profileImage: "https://www.w3schools.com/howto/img_avatar.png"
     },
     {
-      name: "John Doe",
-      employmentTitle: "Manager at Company Name",
+      name: "Aman",
+      employmentTitle: "",
       reviewTitle: "Great service!",
-      reviewBody: "Some great comment about the services offered by the agency. Maybe talk about the quality or quickness or something of that sort. Crisp, interesting comment. Some great comment about the services offered by the agency. Maybe talk about the quality or quickness or something of that sort. Crisp, interesting comment. Some great comment about the services offered by the agency. Maybe talk about the quality or quickness or something of that sort. Crisp, interesting comment. Some great comment about the services offered by the agency. Maybe talk about the quality or quickness or something of that sort. Crisp, interesting comment. Some great comment about the services offered by the agency. Maybe talk about the quality or quickness or something of that sort. Crisp, interesting comment. Some great comment about the services offered by the agency. Maybe talk about the quality or quickness or something of that sort. Crisp, interesting comment. Some great comment about the services offered by the agency. Maybe talk about the quality or quickness or something of that sort. Crisp, interesting comment. ",
-      brandName: "Brand name user",
+      reviewBody: "I've worked with Snigdha and her team for over six months, and would highly recommend them! They are very professional, accommodating, and deadline-oriented. The delivery of SOPs to our company's clients is crucial and an essential part of the admission process. They are extremely knowledgeable about professional writing and placement strategy and have always kept our students happy with their work.",
+      brandName: "Virtual Overseas Consultancy",
+      date: "01/01/2021",
+      profileImage: "https://www.w3schools.com/howto/img_avatar.png"
+    },
+    {
+      name: "Hyma",
+      employmentTitle: "Founder",
+      reviewTitle: "Great service!",
+      reviewBody: "I strongly believe in the power of words, whether they are expressed in writing or verbally. This is why when I saw Snigdhas work, I knew she had this power. I worked with Snigdha on various projects to write content. Be it for Website development, content for profiles, or speeches for beurocrats where they address nations on various topics like sports, politics, or arts, she does her research and uses appropriate words to express the right sentiments and emotions. Her commitment towards her work is commendable and very professional. Thank you so much for always being ready with your 'words' for me, Snigdha ❤️",
+      brandName: " Ikshana Consulting Firm",
       date: "01/01/2021",
       profileImage: "https://www.w3schools.com/howto/img_avatar.png"
     },
