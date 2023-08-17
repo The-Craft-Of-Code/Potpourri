9fa04150e093dd3d1cd40d8cec60f868d723c3fe
sooryasrajan <soorya.s27@gmail.com>
2023-08-18 00:08:38 +0530

    Changed to hash router

--- src/components/router/Router.js
+++ src/components/router/Router.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import App from "../../App";
 import {AboutMe} from "../../pages/AboutMe";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 
 export function RouterComponent() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/about" element={<AboutMe />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
     )
 }
